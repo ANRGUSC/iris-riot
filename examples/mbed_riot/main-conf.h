17273dd769d7152cf39492a3cbfa7537e0a222eb
Jason A. Tran <jasonatran@gmail.com>
2017-03-24 15:42:30 -0700

    mbed_riot: add set channel and set power to main thread

--- examples/mbed_riot/main-conf.h
+++ examples/mbed_riot/main-conf.h
@@ -3,7 +3,9 @@
  */
 
 /* RSSI dump thread port number */
-#define RSSI_DUMP_PORT  9000
+#define RSSI_DUMP_PORT              9000
+
+#define GET_SET_RANGING_THR_PORT    9100
 
 /* for ARREST, set follower's short hwaddr here */
 #define ARREST_LEADER_SHORT_HWADDR "ec:e1"
