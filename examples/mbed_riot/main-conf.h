bf119c677839283cc45a020c72ae46f6a349ee6c
Jason A. Tran <jasonatran@gmail.com>
2017-03-24 21:19:44 -0700

    mbed_riot: implement sound rf ranging receiver

--- examples/mbed_riot/main-conf.h
+++ examples/mbed_riot/main-conf.h
@@ -7,10 +7,25 @@
 
 #define GET_SET_RANGING_THR_PORT    9100
 
+#define 
+
 /* for ARREST, set follower's short hwaddr here */
 #define ARREST_LEADER_SHORT_HWADDR "ec:e1"
 
 #define RSSI_LOCALIZATION_CHAN   26
+
+#define ARREST_LEADER_SOUNDRF_IPV6_ADDR   "fe80::212:4b00:433:ed81"
+
+#define ARREST_LEADER_SOUNDRF_PORT  9200
+
+#define ARREST_LEADER_SOUNDRF_ID    170
+
+#define ARREST_FOLLWOER_RANGE_THR_PORT  9300
+
+#define RANGE_REQ_FLAG      0x12
+#define RANGE_RDY_FLAG      0x34
+#define RANGE_GO_FLAG       0x56
+
 
 
 
