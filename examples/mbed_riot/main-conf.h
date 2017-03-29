29e4be4af3da809ed40ab177ad6af9c57048f755
Jason A. Tran <jasontra@usc.edu>
2017-03-29 14:22:46 -0700

    main-conf.h: switched to new addresses

--- examples/mbed_riot/main-conf.h
+++ examples/mbed_riot/main-conf.h
@@ -8,7 +8,7 @@
 #define GET_SET_RANGING_THR_PORT            9100
 
 /* for ARREST, set follower's short hwaddr here */
-#define ARREST_FOLLOWER_SHORT_HWADDR        "ec:e1"
+#define ARREST_FOLLOWER_SHORT_HWADDR        "e7:e9"
 #define ARREST_LEADER_SHORT_HWADDR          "ed:81"
 #define ARREST_LDR_CTRL_SHORT_HWADDR        "ed:4f"
 
@@ -23,7 +23,7 @@
 
 #define ARREST_FOLLOWER_RANGE_THR_PORT      9300
 
-#define ARREST_FOLLOWER_IPV6_ADDR           "fe80::212:4b00:433:ed4f"
+#define ARREST_FOLLOWER_IPV6_ADDR           "fe80::212:4b00:433:ece1"
 
 #define RANGE_REQ_FLAG                      0x12
 #define RANGE_RDY_FLAG                      0x34
