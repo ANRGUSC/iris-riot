08aa488d6a25bb2cdd00fff92ac25673ee8278fd
Jason A. Tran <jasontra@usc.edu>
2017-06-01 17:42:52 -0700

    add build_test.py and updated hdlc_txvr_test.c

--- examples/mbed_riot/tests/hdlc_txvr_test.c
+++ examples/mbed_riot/tests/hdlc_txvr_test.c
@@ -46,8 +46,6 @@
  * @}
  */
 
-#ifdef ENABLE_HDLC_TXVR_TEST
-
 #include <stdio.h>
 #include <string.h>
 #include <inttypes.h>
@@ -59,86 +57,88 @@
 #include "xtimer.h"
 #include "periph/uart.h"
 #include "hdlc.h"
+#include "dispatcher.h"
+#include "uart_pkt.h"
 
 #define ENABLE_DEBUG (1)
 #include "debug.h"
 
-#ifndef UART_STDIO_DEV
-#define UART_STDIO_DEV          (UART_UNDEF)
-#endif
-
 #define HDLC_PRIO               (THREAD_PRIORITY_MAIN - 1)
+#define THREAD2_PRIO            (THREAD_PRIORITY_MAIN - 1)
 
-static msg_t _hdlc_txvr_test_msg_queue[16];
-static kernel_pid_t hdlc_pid;
+#define MAIN_THR_PORT   1234
+#define THREAD2_PORT    5678
+
+#define PKT_FROM_MAIN_THR   0
+#define PKT_FROM_THREAD2    1
+
+/* see openmote-cc2538's periph_conf.h for second UART pin config */
+
+static msg_t thread2_msg_queue[8];
+static msg_t main_msg_queue[8];
 
 static char hdlc_stack[THREAD_STACKSIZE_MAIN];
+static char dispatcher_stack[THREAD_STACKSIZE_MAIN];
+static char thread2_stack[THREAD_STACKSIZE_MAIN];
 
-static void *_hdlc_txvr_test(void *arg)
+static void *_thread2(void *arg)
 {
     kernel_pid_t hdlc_pid = (kernel_pid_t)arg;
-    /* we need a message queue for the thread running the shell in order to
-     * receive potentially fast incoming packets */
-    msg_init_queue(_hdlc_txvr_test_msg_queue, 16);
+    msg_init_queue(thread2_msg_queue, 8);
+    dispatcher_entry_t thread2 = { NULL, THREAD2_PORT, thread_getpid() };
+    dispatcher_register(&thread2);
 
-    puts("UART INFO:");
-    printf("Available devices:               %i\n", UART_NUMOF);
-    printf("UART used for STDIO (the shell): UART_DEV(%i)\n\n", UART_STDIO_DEV);
-
-    msg_t msg_resp, msg_req;
+    msg_t msg_snd, msg_rcv;
     char frame_no = 0;
+    /* create packets with max size */
     char send_data[HDLC_MAX_PKT_SIZE];
-    char recv_data[HDLC_MAX_PKT_SIZE];
-    hdlc_pkt_t pkt = { .data = send_data, .length = 0 };
-    hdlc_buf_t *buf;
+    hdlc_pkt_t hdlc_snd_pkt =  { .data = send_data, .length = HDLC_MAX_PKT_SIZE };
+    hdlc_pkt_t *hdlc_rcv_pkt;
+    uart_pkt_hdr_t uart_hdr;
 
-    random_init(xtimer_now());
+    /* hdr for each pkt is the same for this test */
+    uart_hdr.src_port = THREAD2_PORT;
+    uart_hdr.dst_port = THREAD2_PORT;
+    uart_hdr.pkt_type = PKT_FROM_THREAD2;
+    uart_pkt_insert_hdr(hdlc_snd_pkt.data, hdlc_snd_pkt.length, &uart_hdr);
 
-    msg_req.type = HDLC_MSG_REG_DISPATCHER;
-    msg_req.content.value = (uint32_t) NULL;
-    msg_send(&msg_req, hdlc_pid);
+    DEBUG("thread2 pid is %" PRIkernel_pid "\n", thread_getpid());
 
-    DEBUG("hdlc_txvr_test pid is %" PRIkernel_pid "\n", thread_getpid());
-
-    msg_req.type = 0;
     int exit = 0;
 
     while(1)
     {
-        pkt.data[0] = frame_no;
+        hdlc_snd_pkt.data[UART_PKT_DATA_FIELD] = frame_no;
 
+        /* fill packet with chars that will not be escaped */
         for(int i = 1; i < HDLC_MAX_PKT_SIZE; i++) {
-            pkt.data[i] = (char) (random_uint32() % 0x7E);
+            hdlc_snd_pkt.data[i] = (char) (random_uint32() % 0x7E);
         }
 
-        pkt.length = HDLC_MAX_PKT_SIZE;
+        DEBUG("main_thr: sending pkt no %d\n", frame_no);
 
-        DEBUG("hdlc_txvr_test: sending pkt no %d\n", frame_no);
-
-        /* send pkt =*/
-        msg_req.type = HDLC_MSG_SND;
-        msg_req.content.ptr = &pkt;
-        msg_send(&msg_req, hdlc_pid);
+        msg_snd.type = HDLC_MSG_SND;
+        msg_snd.content.ptr = &hdlc_snd_pkt;
+        msg_send(&msg_snd, hdlc_pid);
 
         while(1)
         {
-            msg_receive(&msg_resp);
+            msg_receive(&msg_rcv);
 
-            switch (msg_resp.type)
+            switch (msg_rcv.type)
             {
                 case HDLC_RESP_SND_SUCC:
-                    DEBUG("hdlc_txvr_test: sent frame_no %d!\n", frame_no);
+                    DEBUG("thread2: sent frame_no %d!\n", frame_no);
                     exit = 1;
                     break;
                 case HDLC_RESP_RETRY_W_TIMEO:
-                    xtimer_usleep(msg_resp.content.value);
-                    msg_send(&msg_req, hdlc_pid);
+                    xtimer_usleep(msg_rcv.content.value);
+                    msg_send(&msg_snd, hdlc_pid);
                     break;
                 case HDLC_PKT_RDY:
-                    buf = (hdlc_buf_t *)msg_resp.content.ptr;
-                    memcpy(recv_data, buf->data, buf->length);
-                    DEBUG("hdlc_txvr_test: received pkt %d\n", recv_data[0]);
-                    hdlc_pkt_release(buf);
+                    hdlc_rcv_pkt = (hdlc_pkt_t *) msg_rcv.content.ptr;
+                    DEBUG("thread2: received pkt %d\n", hdlc_rcv_pkt->data[UART_PKT_DATA_FIELD]);
+                    hdlc_pkt_release(hdlc_rcv_pkt);
                     break;
                 default:
                     /* error */
@@ -159,18 +159,92 @@ static void *_hdlc_txvr_test(void *arg)
     return 0;
 }
 
-kernel_pid_t txcvr_test_init(char *stack, int stacksize, char priority, const char *name, kernel_pid_t hdlc_thread_pid)
+void main(void)
 {
-    kernel_pid_t res;
+    /* we need a message queue for the thread running the shell in order to
+     * receive potentially fast incoming packets */
+    msg_init_queue(main_msg_queue, 8);
 
-    res = thread_create(stack, stacksize, priority, THREAD_CREATE_STACKTEST, 
-            _hdlc_txvr_test, (void *) hdlc_thread_pid, name);
+    kernel_pid_t hdlc_pid = hdlc_init(hdlc_stack, sizeof(hdlc_stack), HDLC_PRIO, 
+                                      "hdlc", UART_DEV(1));
+    dispacher_init(dispatcher_stack, sizeof(dispatcher_stack), HDLC_PRIO, 
+                   "dispatcher", (void *) (uint32_t) hdlc_pid);
+    thread_create(thread2_stack, sizeof(thread2_stack), THREAD2_PRIO, 
+                  THREAD_CREATE_STACKTEST, _thread2, hdlc_pid, "thread2");
 
-    if (res <= 0) {
-        return -EINVAL;
+    dispatcher_entry_t main_thr = { NULL, MAIN_THR_PORT, thread_getpid() };
+
+    dispatcher_register(&main_thr);
+
+    msg_t msg_snd, msg_rcv;
+    char frame_no = 0;
+    /* create packets with max size */
+    char send_data[HDLC_MAX_PKT_SIZE];
+    hdlc_pkt_t hdlc_snd_pkt =  { .data = send_data, .length = HDLC_MAX_PKT_SIZE };
+    hdlc_pkt_t *hdlc_rcv_pkt;
+    uart_pkt_hdr_t uart_hdr;
+
+    /* hdr for each pkt is the same for this test */
+    uart_hdr.src_port = MAIN_THR_PORT;
+    uart_hdr.dst_port = MAIN_THR_PORT;
+    uart_hdr.pkt_type = PKT_FROM_MAIN_THR;
+    uart_pkt_insert_hdr(hdlc_snd_pkt.data, hdlc_snd_pkt.length, &uart_hdr);
+
+    random_init(xtimer_now().ticks32);
+
+    DEBUG("hdlc_txvr_test pid is %" PRIkernel_pid "\n", thread_getpid());
+
+    int exit = 0;
+
+    while(1)
+    {
+        hdlc_snd_pkt.data[UART_PKT_DATA_FIELD] = frame_no;
+
+        /* fill packet with chars that will not be escaped */
+        for(int i = 1; i < HDLC_MAX_PKT_SIZE; i++) {
+            hdlc_snd_pkt.data[i] = (char) (random_uint32() % 0x7E);
+        }
+
+        DEBUG("main_thr: sending pkt no %d\n", frame_no);
+
+        msg_snd.type = HDLC_MSG_SND;
+        msg_snd.content.ptr = &hdlc_snd_pkt;
+        msg_send(&msg_snd, hdlc_pid);
+
+        while(1)
+        {
+            msg_receive(&msg_rcv);
+
+            switch (msg_rcv.type)
+            {
+                case HDLC_RESP_SND_SUCC:
+                    DEBUG("main_thr: sent frame_no %d!\n", frame_no);
+                    exit = 1;
+                    break;
+                case HDLC_RESP_RETRY_W_TIMEO:
+                    xtimer_usleep(msg_rcv.content.value);
+                    msg_send(&msg_snd, hdlc_pid);
+                    break;
+                case HDLC_PKT_RDY:
+                    hdlc_rcv_pkt = (hdlc_pkt_t *) msg_rcv.content.ptr;
+                    DEBUG("main_thr: received pkt %d\n", hdlc_rcv_pkt->data[UART_PKT_DATA_FIELD]);
+                    hdlc_pkt_release(hdlc_rcv_pkt);
+                    break;
+                default:
+                    /* error */
+                    LED3_ON;
+                    break;
+            }
+
+            if(exit) {
+                exit = 0;
+                break;
+            }
+        }
+
+        frame_no++;
     }
 
-    return res;
+    /* should be never reached */
+    return 0;
 }
-
-#endif /* ENABLE_HDLC_TXVR_TEST */
