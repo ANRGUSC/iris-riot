64f91e36e924c013f5caaf80cd8203e1fa19280a
Daniel <dnlds9@gmail.com>
2017-07-07 11:49:20 -0700

    updated the server script to get client data from the MQTT_SN(modified) broker

--- examples/MQTT_HDLC/tools/server_script.py
+++ examples/MQTT_HDLC/tools/server_script.py
@@ -6,10 +6,18 @@ broker_address="fd00:dead:beef::1"
 port = 1886
 #path_to_certificate="/etc/mosquitto/certs/ca.crt"
 topic_sub = "init_info"
+topic_client_con = "$SYS/broker/clients/connected"
+topic_client_id = "$SYS/broker/clients/ID"
 global topic_pub 
 topic_pub = ""
 server_mqtt = {'0': 'ACK', '1': 'Do something'}
 hwaddr=[]
+global total_num_clients_con_broker
+total_num_clients_con_broker=0
+global connected_clients
+connected_clients=0
+client_ID=[]
+#path_to_certifi
 #Creating the callback functions 
 
 # The callback for when the client receives a CONNACK response from the server.
@@ -24,17 +32,34 @@ def on_connect(client, userdata, rc):
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
+	global topic_pub
+	global total_num_clients_con_broker
+	global connected_clients
 	print ("Data received")
-	print ("Topic: ", msg.topic, "\n" + "Message:" ,(msg.payload.decode()))
 	message = str(msg.payload.decode())
-	if message[0] in server_mqtt:
-		topic_pub=message[1:]
-		hwaddr.append(topic_pub)
-		#when a message is received, the message is published to another topic
-		client.publish(topic_pub,server_mqtt[message[0]])    
+	if msg.topic==topic_sub:		
+			if message[0] in server_mqtt:
+				topic_pub=message[1:]
+				hwaddr.append(topic_pub)
+				#when a message is received, the message is published to another topic
+				client.publish(topic_pub,server_mqtt[message[0]])     
+	elif msg.topic==topic_client_con:
+		connected_clients=connected_clients+1
+		total_num_clients_con_broker=int(message)
+	elif msg.topic==topic_client_id:
+		if str(msg.payload.decode()) in client_ID:
+				connected_clients=connected_clients-1
+		else:
+			client_ID.append(str(msg.payload.decode()))
+	  
+def on_subscribe(mosq, obj, mid, granted_qos):
+	print("Subscribed to all topics ")
 
-def on_publish(client,userdata,result):     #create function for callback
-    print("Data published to topic")
+def on_publish(client,userdata,result): 
+	print("Data published to topic", topic_pub)
+	print("The number of clients connected is",connected_clients)
+	print("The total number of clients that have connected to broker", total_num_clients_con_broker)
+	print("The clients are", client_ID) 
  
 #Creating an instance and setting up the callbacks 
 
@@ -42,10 +67,11 @@ client = mqtt.Client() #creating an instance
 client.on_connect = on_connect	#on_connect callback
 client.on_message = on_message	#on_message callback 
 client.on_publish = on_publish	#on_publish callback	
+client.on_subscribe =on_subscribe
 #client.tls_set(path_to_certificate)	#establishing the SSL certificate
 client.connect(broker_address, port)	#connecting to broker
 
 #Setting up the subscribe 
 
-client.subscribe(topic_sub)	#subscribing to test topic
+client.subscribe([(topic_sub, 0), (topic_client_con, 0), (topic_client_id,0)])
 client.loop_forever()	#looping forever so it doesn't terminate 
