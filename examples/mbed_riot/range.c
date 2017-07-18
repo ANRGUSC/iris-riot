4d060fe872cbe34b29ec65221f866514d21c8d19
richartk <richartk@usc.edu>
2017-07-18 11:39:06 -0700

    comments for range.c

--- examples/mbed_riot/range.c
+++ examples/mbed_riot/range.c
@@ -153,8 +153,11 @@ int range_tx(uint32_t delay_usec)
     gnrc_pktsnip_t *pkt, *hdr;
     gnrc_netif_hdr_t *nethdr;
     uint8_t flags = 0x00;    
-    char buf[BUFFER_SIZE_OF_PACKET] = {0x00, 0x00, 0x00};
-    char buf2[BUFFER_SIZE_OF_PACKET] = {0x00, 0x00, 0x00, 0x00, 0x00};
+    char buf[3] = {0x00, 0x00, 0x00}; // buf used to transmit
+                                      // - tx ranging data
+                                      // - follower nodes' addresses
+    char buf2[BUFFER_SIZE_OF_PACKET] = {0x00, 0x00, 0x00, 0x00, 0x00}; // buf2 used to transmit
+                                                                       // times for sync
 
     int16_t tx_power = TX_POWER;
 
@@ -189,7 +192,8 @@ int range_tx(uint32_t delay_usec)
     xtimer_ticks32_t rstx_start_time;
     uint8_t *hw_addr_nodes[NUM_OF_NODES]; // array of hw_addr of the nodes
     // NETOPT_ADDRESS; // this device's hw_addr 
-    int place_on_list = 0;
+    int place_on_list = 0; // counter to keep track of place on list 
+                           // (also in case not all nodes contacted)
 
     // for determining when to broadcast.
     srand(time(NULL));   // should only be called once
@@ -225,16 +229,18 @@ int range_tx(uint32_t delay_usec)
 
     // ------------------------------------------------------------------------
 
-    // check for leader
+    // check for leader, leader processing
     if(LEADER_HW_ADDR == NETOPT_ADDRESS) //or some matching
     {
         int current_list_size = 1;
         uint16_t list_of_hw_addr[NUM_OF_NODES+1];
         xtimer_ticks32_t cutoff_timer = 0
         xtimer_ticks32_t current_time = xtimer_now();
+        
         // prepare sync packet
         // wait for a little bit for everyone to settle down
         // send SYNC
+        
         // Broadcasting flag setup.
         flags |= GNRC_NETIF_HDR_FLAGS_BROADCAST;
         
@@ -244,11 +250,11 @@ int range_tx(uint32_t delay_usec)
         hw_addr_len = gnrc_netif_addr_from_str(hw_addr, sizeof(hw_addr), RANGE_RX_HW_ADDR);
         
         /* put packet together */
-        buf2[0] = FOLLOW_SYNC;
-        buf2[1] = (uint8_t)current_time >> 24;
-        buf2[2] = (uint8_t)current_time >> 16;
-        buf2[3] = (uint8_t)current_time >> 8;
-        buf2[4] = (uint8_t)current_time;
+        buf2[0] = (uint8_t)current_time >> 24;
+        buf2[1] = (uint8_t)current_time >> 16;
+        buf2[2] = (uint8_t)current_time >> 8;
+        buf2[3] = (uint8_t)current_time;
+        buf2[4] = FOLLOW_SYNC;
 
         pkt = gnrc_pktbuf_add(NULL, &buf2, BUFFER_SIZE_OF_PACKET_LEADER, GNRC_NETTYPE_UNDEF);
         if (pkt == NULL) {
@@ -274,7 +280,9 @@ int range_tx(uint32_t delay_usec)
             return 1;
         }   
 
-        // prepare time (time = NUM_OF_NODES)        
+        // prepare time (time is equal to NUM_OF_NODES)        
+        current_time = xtimer_now();
+        cutoff_timer = xtimer_now() - current_time;
         while(current_list_size < NUM_OF_NODES && cutoff_timer < (NUM_OF_NODES * TIME_WINDOW))
         {
             // set timer for 1 time window
@@ -282,11 +290,10 @@ int range_tx(uint32_t delay_usec)
             if (msg.type == LEAD_INFO) // ensure that they send an INFO signal or something
             {
                 // unpack and get the hw_addr
-                
                 pkt = msg.content.ptr;
                 snip = gnrc_pktsnip_search_type(pkt, GNRC_NETTYPE_UNDEF);
 
-
+                // getting the hw_addr
                 uint16_t incoming_hw_addr = 0x0000;
                 incoming_hw_addr | ((uint16_t)((uint8_t *)snip->data)[0]) << 8;
                 incoming_hw_addr | ((uint8_t *)snip->data)[1];
@@ -302,8 +309,11 @@ int range_tx(uint32_t delay_usec)
             {
                 cutoff_timer = xtimer_now() - current_time;
             }
-        }// done with getting the list
-         // now need to send out the list
+            cutoff_timer = xtimer_now() - current_time; // increment timer
+        }
+
+        // done with getting the list
+        // now need to send out the list
         for(int x = 1; x < current_list_size + 1; x++)
         {
             // prepare and send packets with x and *(x + hw_addr_nodes) and ASSIGN flags.
@@ -314,7 +324,7 @@ int range_tx(uint32_t delay_usec)
             hw_addr_len = gnrc_netif_addr_from_str(hw_addr, sizeof(hw_addr), hw_addr_nodes[x]);
 
             /* put packet together */
-            buf[0] = FOLLOW_ASSIGN;
+            buf[0] = 0x00; // unused
             buf[1] = (uint8_t)x;
             buf[2] = FOLLOW_ASSIGN;
             pkt = gnrc_pktbuf_add(NULL, &buf, 3, GNRC_NETTYPE_UNDEF);
@@ -345,6 +355,7 @@ int range_tx(uint32_t delay_usec)
         {
             // send GO signal with timestamp
             // send SYNC
+            
             // Broadcasting flag setup.
             flags |= GNRC_NETIF_HDR_FLAGS_BROADCAST;
             
@@ -354,11 +365,11 @@ int range_tx(uint32_t delay_usec)
             hw_addr_len = gnrc_netif_addr_from_str(hw_addr, sizeof(hw_addr), RANGE_RX_HW_ADDR);
             
             /* put packet together */
+            buf2[0] = (uint8_t)current_time >> 24;
+            buf2[1] = (uint8_t)current_time >> 16;
+            buf2[2] = (uint8_t)current_time >> 8;
+            buf2[3] = (uint8_t)current_time;
             buf2[0] = FOLLOW_GO;
-            buf2[1] = (uint8_t)current_time >> 24;
-            buf2[2] = (uint8_t)current_time >> 16;
-            buf2[3] = (uint8_t)current_time >> 8;
-            buf2[4] = (uint8_t)current_time;
 
             pkt = gnrc_pktbuf_add(NULL, &buf2, BUFFER_SIZE_OF_PACKET_LEADER, GNRC_NETTYPE_UNDEF);
             if (pkt == NULL) {
@@ -387,13 +398,17 @@ int range_tx(uint32_t delay_usec)
             xtimer_usleep( (current_list_size + 1) * TIME_WINDOW );
         }
     }
+    // end leader processing
+    // leader should never go beyond this
     
+    // follower code starts here, should skip leader processing
     while(true)
     {
         msg_receive(&msg);
         switch (msg.type)
         {
             case FOLLOW_SYNC:
+            	// FOLLOW_SYNC = {time[4], FOLLOW_SYNC}
                 // - All nodes sync to timestamp.
 
                 // Pull time from FOLLOW_SYNC from the leader node.
@@ -402,14 +417,14 @@ int range_tx(uint32_t delay_usec)
                 /* get snip containing packet data where we put the packet number */
                 snip = gnrc_pktsnip_search_type(pkt, GNRC_NETTYPE_UNDEF);
 
-                // uint8_t = 0x00; // == char
-                // uint32_t = 0x00000000;
-                // data from leader with this signal will run from 0 to 4, 1-4 holds the 
-                // address. 
-                // sloppy, see if this works
+                // leader sends out pkt with FOLLOW_SYNC
+                // should be 5 bytes big
+                // bytes 0-3 should hold the time
+                // byte 4 holds the identifier? flag? (that it IS a FOLLOW_SYNC signal) 
+                // sloppy, should've used strcpy? or something to copy the bits over.
                 for(int x = 0; x < 4; x++)
                 {
-                    part_of_time = ((uint8_t *) snip->data)[x+1];
+                    part_of_time = ((uint8_t *) snip->data)[x];
                     time_processing = (time_processing << 8) | part_of_time;
                 }
                 gnrc_pktbuf_release(pkt);
@@ -423,20 +438,18 @@ int range_tx(uint32_t delay_usec)
                 xtimer_usleep(r * TIME_WINDOW);
 
                 // TODO: Double check this.
-                // Broadcasting flag setup.
-                //flags |= GNRC_NETIF_HDR_FLAGS_BROADCAST;
-                
+                // TODO: code in collision handling.
+
                 /** Send L2 Packet **/
                 /* network interface */
                 dev = ifs[0];
-                // hw_addr_len = gnrc_netif_addr_from_str(hw_addr, sizeof(hw_addr), RANGE_RX_HW_ADDR);
                 hw_addr_len = gnrc_netif_addr_from_str(hw_addr, sizeof(hw_addr), LEADER_HW_ADDR);
                 
                 /* put packet together */
-                // add in appropriate flags, collision checking
-                buf[0] = LEAD_INFO;
-                buf[1] = tx_node_id1;
-                buf[2] = tx_node_id2;
+                buf[0] = tx_node_id1;
+                buf[1] = tx_node_id2;
+                buf[2] = LEAD_INFO;
+
                 pkt = gnrc_pktbuf_add(NULL, &buf, sizeof(buf), GNRC_NETTYPE_UNDEF);
                 if (pkt == NULL) {
                     DEBUG("error: packet buffer full");
@@ -449,6 +462,7 @@ int range_tx(uint32_t delay_usec)
                     gnrc_pktbuf_release(pkt);
                     return 1;
                 }
+
                 LL_PREPEND(pkt, hdr);
                 nethdr = (gnrc_netif_hdr_t *)hdr->data;
                 nethdr->flags = flags;
@@ -462,7 +476,9 @@ int range_tx(uint32_t delay_usec)
                 }   
                 break;
             case FOLLOW_ASSIGN:
+                // FOLLOW_ASSIGN = {0x00, place_on_list, FOLLOW_ASSIGN}
                 // - Nodes remember place_on_list variable.
+                
                 pkt = msg.content.ptr;
 
                 /* get snip containing packet data where we put the packet number */
@@ -473,23 +489,24 @@ int range_tx(uint32_t delay_usec)
                 gnrc_pktbuf_release(pkt);
                 break;
             case FOLLOW_GO:
-                // syncing...//
+            	// FOLLOW_GO = {time[4], FOLLOW_GO}
+                // syncing...
                 // - All nodes sync to timestamp.
 
-                // Pull time from FOLLOW_SYNC from the leader node.
+                // Pull time from FOLLOW_GO from the leader node.
                 pkt = msg.content.ptr;
 
                 /* get snip containing packet data where we put the packet number */
                 snip = gnrc_pktsnip_search_type(pkt, GNRC_NETTYPE_UNDEF);
 
-                // uint8_t = 0x00; // == char
-                // uint32_t = 0x00000000;
-                // data from leader with this signal will run from 0 to 4, 1-4 holds the 
-                // address. 
-                // sloppy, see if this works
+                // leader sends out pkt with FOLLOW_GO
+                // should be 5 bytes big
+                // bytes 0-3 should hold the time
+                // byte 4 holds the identifier? flag? (that it IS a FOLLOW_GO signal) 
+                // sloppy, should've used strcpy? or something to copy the bits over.
                 for(int x = 0; x < 4; x++)
                 {
-                    part_of_time = ((uint8_t *) snip->data)[x+1];
+                    part_of_time = ((uint8_t *) snip->data)[x];
                     time_processing = (time_processing << 8) | part_of_time;
                 }
                 gnrc_pktbuf_release(pkt);
@@ -550,7 +567,7 @@ int range_tx(uint32_t delay_usec)
                 break;
     }
 
-    // main loop
+    // old main loop
     // while(true){
     // for(int x = 0; x < MAX_TX_RUNS; x++){
 
