466cb016ab1a813d9866887f88106a8302e5b9db
richartk <richartk@usc.edu>
2017-07-19 12:36:30 -0700

    updated localization_test.c

--- examples/mbed_riot/tests/localization_test.c
+++ examples/mbed_riot/tests/localization_test.c
@@ -7,6 +7,7 @@
  *
  * Contributors:
  * Yutong Gu
+ * Jason A. Tran
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy 
  * of this software and associated documentation files (the "Software"), to deal
