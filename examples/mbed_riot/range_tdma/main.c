01f560be726b4342f089219918af9ce392bc6a19
richartk <richartk@usc.edu>
2017-08-08 13:01:19 -0700

    Revised code, took out start_time and wait_time to look cleaner.

--- examples/mbed_riot/range_tdma/main.c
+++ examples/mbed_riot/range_tdma/main.c
@@ -106,7 +106,9 @@ int main(void)
     // Miscellaneous
     //------------------------------------------------------------------------//
     bool wait_for_id = true;
-    xtimer_ticks32_t go_time; // go_time = time at which openmote sends ranging.
+    // go_time is the time to send the signal. Update to xtimer_now + 
+    // (id - incoming id) * tdma_slot_time_msec.
+    xtimer_ticks32_t go_time;
     uint32_t id_delay_time;
     uint8_t incoming_rank;
     // NETOPT_ADDRESS; // this device's hw_addr 
@@ -157,48 +159,50 @@ int main(void)
         msg_receive(&msg);
         switch (msg.type)
         {
-            // NODE receives ID packet from LEADER.
-            case GNRC_NETAPI_MSG_TYPE_RCV:
-                DEBUG("Received RCV pkt.\n");
-                recv_pkt = msg.content.ptr;
+            msg_receive(&msg);
+            switch (msg.type)
+            {
+                // NODE receives ID packet from LEADER.
+                case GNRC_NETAPI_MSG_TYPE_RCV:
+                    DEBUG("Received RCV pkt.\n");
+                    recv_pkt = msg.content.ptr;
 
-                /* first snip should be of type GNRC_NETTYPE_UNDEF carrying the data */
-                if (((uint8_t *)recv_pkt->data)[0] == ((TDMA_ANCHOR_ID_RESP_U16_FLAG >> 8) & 0xFF) &&
-                    ((uint8_t *)recv_pkt->data)[1] == (TDMA_ANCHOR_ID_RESP_U16_FLAG & 0xFF) )
-                {
-                    DEBUG("Was ID packet.\n");
-                    // NODE records its ranking.
-                    anchor_node_id = ((uint8_t *)recv_pkt->data)[2];
-                    DEBUG("Rank: ");
-                    DEBUG("%d\n", anchor_node_id);
-                    // NODE syncs with the LEADER.
-                    tdma_slot_time_msec &= 0x00;
-                    tdma_slot_time_msec |= ((uint8_t *)recv_pkt->data)[3];
-                    tdma_slot_time_msec = tdma_slot_time_msec << 8;
-                    tdma_slot_time_msec |= ((uint8_t *)recv_pkt->data)[4];
+                    /* first snip should be of type GNRC_NETTYPE_UNDEF carrying the data */
+                    if (((uint8_t *)recv_pkt->data)[0] == ((TDMA_ANCHOR_ID_RESP_U16_FLAG >> 8) & 0xFF) &&
+                        ((uint8_t *)recv_pkt->data)[1] == (TDMA_ANCHOR_ID_RESP_U16_FLAG & 0xFF) )
+                    {
+				        DEBUG("Was ID packet.\n");
+                        // NODE records its ranking.
+                        anchor_node_id = ((uint8_t *)recv_pkt->data)[2];
+                        DEBUG("Rank: ");
+                        DEBUG("%d\n", anchor_node_id);
+                        // NODE syncs with the LEADER.
+                        tdma_slot_time_msec &= 0x00;
+                        tdma_slot_time_msec |= ((uint8_t *)recv_pkt->data)[3];
+                        tdma_slot_time_msec = tdma_slot_time_msec << 8;
+                        tdma_slot_time_msec |= ((uint8_t *)recv_pkt->data)[4];
 
                     // NODE records total number of nodes (useful for NODE 1).
                     total_num_anchors = ((uint8_t *)recv_pkt->data)[5];
 
-                    // NODE A received the data correctly and now breaks out of the loop.
-                    wait_for_id = false;
-                }
-                gnrc_pktbuf_release(recv_pkt);
-                break;
-            case GNRC_NETAPI_MSG_TYPE_SND:
-                /* This thread will get all send l2 send requests, even if it's
-                coming from this thread. Discard it */
-                DEBUG("Received SND pkt (1).\n");
-                gnrc_pktbuf_release((gnrc_pktsnip_t *)msg.content.ptr);
-                break;
-            default:
-                // pkt received didn't match any of these.
-                DEBUG("msg.type = %d\n", msg.type);
-                DEBUG("Waiting for ID packet from LEADER. PKT was none of these.\n");
-                break;
+                        // NODE A received the data correctly and now breaks out of the loop.
+                        wait_for_id = false;
+                    }
+                    break;
+                case GNRC_NETAPI_MSG_TYPE_SND:
+                    /* This thread will get all send l2 send requests, even if it's
+                    coming from this thread. Discard it */
+                    DEBUG("Received SND pkt (1).\n");
+                    gnrc_pktbuf_release((gnrc_pktsnip_t *)msg.content.ptr);
+                    break;
+                default:
+                    // pkt received didn't match any of these.
+                    DEBUG("msg.type = %d\n", msg.type);
+                    DEBUG("Waiting for ID packet from LEADER. PKT was none of these.\n");
+                    break;
+            }
         }
-    }
-    //------------------------------------------------------------------------//
+        //--------------------------------------------------------------------//
 
 // MAIN LOOP
     while (true)
@@ -220,13 +224,13 @@ int main(void)
                     if ( ((uint8_t *)recv_pkt->data)[0] == RANGE_FLAG_BYTE0 &&
                          ((uint8_t *)recv_pkt->data)[1] == RANGE_FLAG_BYTE1 ) 
                     {
-                        DEBUG("Another anchor node ranging packet.\n");
+	                    DEBUG("Another anchor node ranging packet.\n");
                         incoming_rank = ((uint8_t *)recv_pkt->data)[1];
                         // NODE's rank is 1 and listening for the last NODE.
                         if ( (incoming_rank == total_num_anchors) && (anchor_node_id == 1))
                         {
                             DEBUG("last rank to first.\n");
-                            go_time.ticks32 = xtimer_now().ticks32 + (uint32_t)tdma_slot_time_msec;
+                            go_time.ticks32 = xtimer_now.ticks32 + (uint32_t)tdma_slot_time_msec;
                         }
                         // NODE's rank is higher than the incoming NODE's transmission.
                         if (incoming_rank < anchor_node_id)
