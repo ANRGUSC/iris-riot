9175e625d66790bfc496a683bbb8e9a1678ab5b4
richartk <richartk@usc.edu>
2017-08-15 13:43:12 -0700

    first start on the observer

---
+++ examples/observer/main-conf.h
@@ -0,0 +1,52 @@
+/**
+ * All macros for network addresses
+ */
+
+/* RSSI dump thread port number */
+#define RSSI_DUMP_PORT                      9000
+
+#define GET_SET_RANGING_THR_PORT            9100
+
+/* for ARREST, set follower's short hwaddr here */
+#define ARREST_FOLLOWER_SHORT_HWADDR        "e7:e9"
+#define ARREST_LEADER_SHORT_HWADDR          "ed:81"
+#define ARREST_LDR_CTRL_SHORT_HWADDR        "ed:4f"
+
+#define RSSI_LOCALIZATION_CHAN              26
+#define ARREST_DATA_CHANNEL                 25
+
+#define ARREST_LEADER_LONG_HWADDR           "00:12:4b:00:04:33:ed:81"
+#define ARREST_LEADER_SOUNDRF_IPV6_ADDR     "fe80::212:4b00:433:ed5e"
+
+#define ARREST_LEADER_SOUNDRF_PORT          9200
+
+#define ARREST_LEADER_SOUNDRF_ID            170
+
+#define ARREST_FOLLOWER_RANGE_THR_PORT      9300
+
+#define ARREST_FOLLOWER_IPV6_ADDR           "fe80::212:4b00:433:eca3"
+
+#define RANGE_REQ_FLAG                      0x12
+#define RANGE_RDY_FLAG                      0x34
+#define RANGE_GO_FLAG                       0x56
+
+#define DEFAULT_ULTRASOUND_THRESH           45
+#define MAX_SOUND_SAMPLES                   500
+
+#define REMOTE_CTRL_FLAG                    0x99 /* 10011001 */
+
+#define RANGE_REQ_TIMEO_USEC                1000000
+
+/* TDMA Localization Settings */
+/* sent in network byte order (big-endian) */
+#define TDMA_ANCHOR_ID_REQ_U16_FLAG         0x5444  /* 'T' and 'D' for TDma */
+#define TDMA_ANCHOR_ID_RESP_U16_FLAG             0x4d41  /* 'M' and 'A' for tdMA */
+#define TDMA_SLOT_TIME_USEC                 200000
+#define TDMA_BOOTSTRAP_CHANNEL              11
+#define TDMA_TOTAL_ANCHOR_NODES             4       /* experiment specific */
+
+
+
+
+
+
---
+++ examples/observer/uart_pkt.h
@@ -0,0 +1,92 @@
+/**
+ * Copyright (c) 2017, Autonomous Networks Research Group. All rights reserved.
+ * Developed by:
+ * Autonomous Networks Research Group (ANRG)
+ * University of Southern California
+ * http://anrg.usc.edu/
+ *
+ * Contributors:
+ * Jason A. Tran
+ * Pradipta Ghosh
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy 
+ * of this software and associated documentation files (the "Software"), to deal
+ * with the Software without restriction, including without limitation the 
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
+ * sell copies of the Software, and to permit persons to whom the Software is 
+ * furnished to do so, subject to the following conditions:
+ * - Redistributions of source code must retain the above copyright notice, this
+ *     list of conditions and the following disclaimers.
+ * - Redistributions in binary form must reproduce the above copyright notice, 
+ *     this list of conditions and the following disclaimers in the 
+ *     documentation and/or other materials provided with the distribution.
+ * - Neither the names of Autonomous Networks Research Group, nor University of 
+ *     Southern California, nor the names of its contributors may be used to 
+ *     endorse or promote products derived from this Software without specific 
+ *     prior written permission.
+ * - A citation to the Autonomous Networks Research Group must be included in 
+ *     any publications benefiting from the use of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ * CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS WITH 
+ * THE SOFTWARE.
+ */
+
+/**
+ * @file        uart_pkt.h
+ * @brief       Helper library for creating packets to be over UART via hdlc.
+ *
+ * @author      Jason A. Tran <jasontra@usc.edu>
+ * @author      Pradipta Ghosh <pradiptg@usc.edu>
+ * 
+ */
+
+#ifndef UART_PKT_H_
+#define UART_PKT_H_
+
+#define UART_PKT_HDR_LEN            5
+#define UART_PKT_TYPE_FIELD         4
+#define UART_PKT_DATA_FIELD         5
+
+typedef struct __attribute__((packed)) {
+    uint16_t    src_port;      
+    uint16_t    dst_port;      
+    uint8_t     pkt_type;
+} uart_pkt_hdr_t;
+
+/**
+ * @brief Message types from mbed-os to riot-os
+ */
+typedef enum {
+    RADIO_SET_CHAN          = 0,
+    RADIO_SET_POWER         = 1,
+    SOUND_RANGE_REQ         = 2,
+    SOUND_RANGE_X10_REQ     = 3,
+    RSSI_DUMP_START         = 4,
+    RSSI_DUMP_STOP          = 5
+} mbed_to_riot_t;
+
+/**
+ * @brief Message types from riot-os to mbed-os
+ */
+typedef enum  {
+    RADIO_SET_CHAN_SUCCESS  = 0,
+    RADIO_SET_CHAN_FAIL     = 1,
+    RADIO_SET_POWER_SUCCESS = 2,
+    RADIO_SET_POWER_FAIL    = 3,
+    SOUND_RANGE_DONE        = 4,
+    RSSI_SCAN_STARTED       = 5,
+    RSSI_SCAN_STOPPED       = 6,
+    RSSI_DATA_PKT           = 7,
+    RADIO_FWD_UDP_PKT       = 8
+} riot_to_mbed_t;
+
+void *uart_pkt_insert_hdr(void *buf, size_t buf_len, const uart_pkt_hdr_t *hdr);
+size_t uart_pkt_cpy_data(void *buf, size_t buf_len, const void *data, size_t data_len);
+int uart_pkt_parse_hdr(uart_pkt_hdr_t *dst_hdr, const void *src,  size_t src_len);
+void *uart_pkt_get_data(void *src, size_t src_len);
+
+#endif /* UART_PKT_H_ */
