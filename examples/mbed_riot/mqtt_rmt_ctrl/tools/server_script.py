68af86109b4b5f532766e6c5352e1b48d528bad8
Daniel <dnlds9@gmail.com>
2017-09-24 15:21:05 -0700

    cleaned the code for the rmt_ctrl python script

--- examples/mbed_riot/mqtt_rmt_ctrl/tools/server_script.py
+++ examples/mbed_riot/mqtt_rmt_ctrl/tools/server_script.py
@@ -16,6 +16,7 @@ global topic_pub
 topic_pub = ""
 
 global data_pub
+data_pub = "1test/trial"
 
 global rmt_go
 rmt_go = 0
@@ -23,11 +24,13 @@ rmt_go = 0
 global rmt_ctrl_start
 rmt_ctrl_start = True
 
-client_ID=[]
+client_ID = []
 topic_sub = "init_info"
 topic_rmt = "test/trial"
+
 toggle_client = []
 toggle_client_value = [True] * 5
+
 msg_type= { 'HW_ADDR' : '0', 'REQUEST' : '1', 'SEND_RSSI' : '2', 'LEN_CLIENTS_LIST' : '3', 'GET_CLIENTS' : '4', 'UDP_SEND' : '5'}
 
 server_mqtt = {'0': 'ACK', '1': 'Do something'}
@@ -40,69 +43,82 @@ def rmt_ctrl():
                     break
 
 def main(stdscr):
+    # use 'q' to quit the application 
     # do not wait for input when calling getch
     stdscr.nodelay(1)
     stdscr.clear()
     while True:
         # get keyboard input, returns -1 if none available        
         c = stdscr.getch()
-        
+
+        #if a key has been pressed
         if c != -1:
             stdscr.clear()
             # print numeric value
-            if str(c) == "97":
+            if str(c) == "97": #value of a
+                #if all nodes are active send to shared topic
                 if (toggle_client_value == [True] * 5):
                     client.publish(topic_rmt,"8a")
+                #sending to a subset of nodes(active)
                 else :
                     for i in range (len(toggle_client_value)):
                         if toggle_client_value[i] == True:
                             client.publish(toggle_client[i],"8a")
                 stdscr.addstr("left")
-            elif str(c) =="115":
+
+            elif str(c) =="115": #value of s
+                #if all nodes are active send to shared topic
                 if (toggle_client_value == [True] * 5):
                     client.publish(topic_rmt,"8s")
+                #sending to a subset of nodes(active)
                 else :
                     for i in range (len(toggle_client_value)):
                         if toggle_client_value[i] == True:
                             client.publish(toggle_client[i],"8s")
                 stdscr.addstr("backward")
-            elif str(c) =="100":
+
+            elif str(c) =="100": #value of d
+                #if all nodes are active send to shared topic
                 if (toggle_client_value == [True] * 5):
                     client.publish(topic_rmt,"8d")
+                #sending to a subset of nodes(active)
                 else :
                     for i in range (len(toggle_client_value)):
                         if toggle_client_value[i] == True:
                             client.publish(toggle_client[i],"8d")
                 stdscr.addstr("right")
-            elif str(c) == "119":
+
+            elif str(c) == "119": #value of w
+                #if all nodes are active send to shared topic
                 if (toggle_client_value == [True] * 5):
                     client.publish(topic_rmt,"8w")
+                #sending to a subset of nodes(active) 
                 else :
                     for i in range (len(toggle_client_value)):
                         if toggle_client_value[i] == True:
                             client.publish(toggle_client[i],"8w")
-                stdscr.addstr("forward")            
-            elif str(c) == "49":  
+                stdscr.addstr("forward")  
+
+            #handles toggling nodes on or off 
+            elif str(c) == "49":  #use '1' on the keyboard to toggle the first node
                 toggle_client_value[((int(str(c)))-49)] = not toggle_client_value[((int(str(c)))-49)]
                 stdscr.addstr("toggle 1st node")
-            elif str(c) == "50":
+            elif str(c) == "50": #use '2' on the keyboard to toggle the second node
                 toggle_client_value[((int(str(c)))-49)] = not toggle_client_value[((int(str(c)))-49)]
                 stdscr.addstr("toggle 2nd node")
-            elif str(c) == "51":
+            elif str(c) == "51": #use '3' on the keyboard to toggle the third node
                 toggle_client_value[((int(str(c)))-49)] = not toggle_client_value[((int(str(c)))-49)]
                 stdscr.addstr("toggle 3rd node")
-            elif str(c) == "52":
+            elif str(c) == "52": #use '4' on the keyboard to toggle the fourth node
                 toggle_client_value[((int(str(c)))-49)] = not toggle_client_value[((int(str(c)))-49)]
                 stdscr.addstr("toggle 4th node")    
-            elif str(c) == "53":
+            elif str(c) == "53": #use '5' on the keyboard to toggle the fifth node
                 toggle_client_value[((int(str(c)))-49)] = not toggle_client_value[((int(str(c)))-49)]
                 stdscr.addstr("toggle 5th node")
-            elif str(c) == "113":
+            #handles exitting the curses screen
+            elif str(c) == "113": #value of 'q'
                 curses.endwin()
                 break
-
-
-
 
             else:
                 continue
@@ -124,6 +140,7 @@ def on_connect(client, userdata, rc):
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     global clients_connected
+    global data_pub
     print ("Data received")
     message = str(msg.payload.decode())
     if message[0] == msg_type['HW_ADDR']:
@@ -134,7 +151,9 @@ def on_message(client, userdata, msg):
             clients_connected += 1
 
         #when a message is received, the message is published to another topic
-        client.publish(topic_pub,server_mqtt[message[0]])                
+        client.publish(topic_pub,server_mqtt[message[0]])  
+        if (rmt_go == 1):
+            client.publish(client_ID[(len(client_ID)-1)], data_pub)              
         print("The number of clients connected is", clients_connected)
         print("The clients are", client_ID)
     else:
@@ -165,19 +184,20 @@ data_pub = ""
 
 client.loop_start() 
 
+#starting thread for remote control 
 t = Thread(target=rmt_ctrl)
 t.start()  
 
-data_pub = "1test/trial"
 
 while 1:
 
-    if clients_connected == 5 and rmt_ctrl_start:
+    if (clients_connected == 5 and rmt_ctrl_start):
         time.sleep(2)
         print ("Ready to start")
         for i in range(clients_connected):
             client.publish(client_ID[i],data_pub)
             time.sleep(0.5)
+        print ("Use \'q\' to exit the curses screen")
         time.sleep(5)
         toggle_client = client_ID
         print("execute ncurses main")
