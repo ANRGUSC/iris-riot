76bbded5e3d9dac463d18b1b78143765fe7c92d7
Daniel <dnlds9@gmail.com>
2017-09-25 16:51:25 -0700

    Cleaned the server_script code

---
+++ examples/mbed_riot/mqtt_rmt_ctrl/main.c
@@ -0,0 +1,186 @@
+/**
+ * Copyright (c) 2017, Autonomous Networks Research Group. All rights reserved.
+ * Developed by:
+ * Autonomous Networks Research Group (ANRG)
+ * University of Southern California
+ * http://anrg.usc.edu/
+ *
+ * Contributors:
+ * Pradipta Ghosh
+ * Daniel Dsouza
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy 
+ * of this software and associated documentation files (the "Software"), to deal
+ * with the Software without restriction, including without limitation the 
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
+ * sell copies of the Software, and to permit persons to whom the Software is 
+ * furnished to do so, subject to the following conditions:
+ * - Redistributions of source code must retain the above copyright notice, this
+ *     list of conditions and the following disclaimers.
+ * - Redistributions in binary form must reproduce the above copyright notice, 
+ *     this list of conditions and the following disclaimers in the 
+ *     documentation and/or other materials provided with the distribution.
+ * - Neither the names of Autonomous Networks Research Group, nor University of 
+ *     Southern California, nor the names of its contributors may be used to 
+ *     endorse or promote products derived from this Software without specific 
+ *     prior written permission.
+ * - A citation to the Autonomous Networks Research Group must be included in 
+ *     any publications benefiting from the use of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ * CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS WITH 
+ * THE SOFTWARE.
+ */
+
+/**
+ * @ingroup     examples
+ * @{
+ *
+ * @file
+ * @brief       Full-duplex MQTT test
+ *
+ *
+ * @author      Pradipta Ghosh <pradiptg@usc.edu>
+ * @author      Daniel Dsouza <dmdsouza@usc.edu>
+ * @}
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <inttypes.h>
+
+#include "board.h"
+#include "thread.h"
+#include "msg.h"
+#include "random.h"
+#include "xtimer.h"
+#include "periph/uart.h"
+#include "net/hdlc.h"
+#include "net/uart_pkt.h"
+
+#include "net/gnrc.h"
+#include "net/gnrc/netapi.h"
+#include <stdlib.h>
+#include "net/emcute.h"
+#include "net/ipv6/addr.h"
+#include "mqtt_thread.h"
+
+#include "mqtt.h"
+#include "main-conf.h"
+
+#define ENABLE_DEBUG (1)
+#include "debug.h"
+//setting the priority of the hdlc and thread2
+#define HDLC_PRIO               (THREAD_PRIORITY_MAIN - 1)
+#define THREAD2_PRIO            (THREAD_PRIORITY_MAIN)
+//setting the port of the main thread
+//setting the port of thread2 
+
+/* see openmote-cc2538's periph_conf.h for second UART pin config */
+//setting the message queue with message structs
+static msg_t main_msg_queue[16];
+//creating the stacks
+static char hdlc_stack[THREAD_STACKSIZE_MAIN + 512];//16896
+static char thread2_stack[THREAD_STACKSIZE_MAIN];//16384
+
+
+int main(void)
+{
+    /*Getting the hardware address*/
+
+    
+    /* we need a message queue for the thread running the shell in order to
+     * receive potentially fast incoming packets */
+    msg_init_queue(main_msg_queue, 16);
+    //intializing the port and pid 
+    hdlc_entry_t main_thr = { NULL, MAIN_THR_PORT, thread_getpid() };
+    //registering the main_thr to the list 
+    hdlc_register(&main_thr);
+    //setting the hdlc pid 
+    kernel_pid_t hdlc_pid = hdlc_init(hdlc_stack, sizeof(hdlc_stack), HDLC_PRIO, 
+                                      "hdlc", UART_DEV(0));
+    
+    kernel_pid_t mqtt_pid = mqtt_thread_init(thread2_stack, sizeof(thread2_stack), THREAD2_PRIO, 
+                                      "thread2", (void*) hdlc_pid);
+
+    
+    //The main thread DOES NOT send and receive messages in this example
+    //setting up the two message structs 
+    msg_t msg_snd, msg_rcv;
+    char frame_no = 0;
+    //create packets with max size 
+    char send_data[HDLC_MAX_PKT_SIZE];//size 16
+    hdlc_pkt_t hdlc_snd_pkt =  { .data = send_data, .length = HDLC_MAX_PKT_SIZE };
+    hdlc_pkt_t *hdlc_rcv_pkt;
+    uart_pkt_hdr_t uart_hdr;
+    void *main_mbed_rcv_ptr;
+    mqtt_pkt_t *main_rcv_pkt;
+
+    // hdr for each pkt is the same for this test 
+    uart_hdr.src_port = MAIN_THR_PORT;
+    uart_hdr.dst_port = MAIN_THR_PORT;
+    uart_hdr.pkt_type = MQTT_PKT_TYPE;
+    uart_pkt_insert_hdr(hdlc_snd_pkt.data, hdlc_snd_pkt.length, &uart_hdr);
+
+    random_init(xtimer_now().ticks32);
+
+    //DEBUG("Main Thread pid is %" PRIkernel_pid "\n", thread_getpid());
+
+    int exit = 0;
+
+    while(1)
+    {        
+        while(1)
+        {
+            msg_receive(&msg_rcv);
+            DEBUG("Waiting for a message\n");
+            switch (msg_rcv.type)
+            {
+                case HDLC_RESP_SND_SUCC:
+                    DEBUG("main_thr: sent frame_no %d!\n", frame_no);
+                    exit = 1;
+                    break;
+                case HDLC_RESP_RETRY_W_TIMEO:
+                    xtimer_usleep(msg_rcv.content.value);
+                    DEBUG("main_thr: retrying frame_no %d\n", frame_no);
+                    if(!msg_try_send(&msg_snd, hdlc_pid)) {
+                        DEBUG("main_thr: HDLC msg queue full!\n");
+                        msg_send_to_self(&msg_rcv);
+                    }
+
+                    break;
+                case HDLC_PKT_RDY:
+                /*
+                    hdlc_rcv_pkt = (hdlc_pkt_t *) msg_rcv.content.ptr;
+                    printf("The \n packet \n has \n been received\n");  
+                    main_mbed_rcv_ptr = hdlc_rcv_pkt->data + UART_PKT_DATA_FIELD;
+                    main_rcv_pkt = (mqtt_pkt_t *)main_mbed_rcv_ptr;
+                    printf("The data received is %s \n", main_rcv_pkt->data);
+                    printf("The topic received is %s \n", main_rcv_pkt->topic);
+                    break;
+                    */
+                default:
+                    //error
+                    LED3_ON;
+                    break;
+            }
+
+            if(exit) {
+                exit = 0;
+                break;
+            }
+        }
+
+        frame_no++;
+
+        //control transmission rate via interpacket intervals 
+        xtimer_usleep(10000);
+    }
+    //should be never reached 
+    
+    return 0;
+}
+
---
+++ examples/mbed_riot/mqtt_rmt_ctrl/tools/server_script.py
@@ -0,0 +1,148 @@
+import paho.mqtt.client as mqtt
+import time
+from threading import Thread
+import curses
+#initializing the variables
+#Change the following according to your system 
+broker_address="fd00:dead:beef::1" 
+port = 1886
+
+global count
+
+global clients_connected
+clients_connected = 0
+
+global topic_pub 
+topic_pub = ""
+
+global data_pub
+
+global rmt_go
+rmt_go = 0
+
+global rmt_ctrl_start
+rmt_ctrl_start = True
+
+client_ID=[]
+topic_sub = "init_info"
+topic_rmt = "test/trial"
+
+msg_type= { 'HW_ADDR' : '0', 'REQUEST' : '1', 'SEND_RSSI' : '2', 'LEN_CLIENTS_LIST' : '3', 'GET_CLIENTS' : '4', 'UDP_SEND' : '5'}
+
+server_mqtt = {'0': 'ACK', '1': 'Do something'}
+
+def rmt_ctrl():
+    while 1:        
+        if rmt_go == 1:        
+            if __name__ == '__main__':
+                    curses.wrapper(main)
+                    break
+
+def main(stdscr):
+    # do not wait for input when calling getch
+    stdscr.nodelay(1)
+    stdscr.clear()
+    while True:
+        # get keyboard input, returns -1 if none available        
+        c = stdscr.getch()
+        
+        if c != -1:
+            stdscr.clear()
+            # print numeric value
+            if str(c) == "97":
+                client.publish(topic_rmt,"8a")
+                stdscr.addstr("left")
+            elif str(c) =="115":
+                client.publish(topic_rmt,"8s")
+                stdscr.addstr("backward")
+            elif str(c) =="100":
+                client.publish(topic_rmt,"8d")
+                stdscr.addstr("right")
+            elif str(c) == "119":
+                client.publish(topic_rmt,"8w")
+                stdscr.addstr("forward")
+            elif str(c) == "113":
+                curses.endwin()
+                break
+
+            else:
+                continue
+            stdscr.refresh()
+
+            # return curser to start position
+            stdscr.move(0, 0)
+
+
+#Creating the callback functions
+
+# The callback for when the client receives a CONNACK response from the server.
+# The callback function when client is connected to the broker.
+
+def on_connect(client, userdata, rc):
+    print ("Connected to broker")
+
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    global clients_connected
+    print ("Data received")
+    message = str(msg.payload.decode())
+    if message[0] == msg_type['HW_ADDR']:
+        topic_pub = message[1:]
+        print("The topic is \n", topic_pub)
+        if topic_pub not in client_ID:
+            client_ID.append(topic_pub)
+            clients_connected += 1
+
+        #when a message is received, the message is published to another topic
+        client.publish(topic_pub,server_mqtt[message[0]])                
+        print("The number of clients connected is", clients_connected)
+        print("The clients are", client_ID)
+    else:
+        print ("wrong format %d\n" % int(message[0]))
+                                        
+
+def on_subscribe(mosq, obj, mid, granted_qos):
+   print("Subscribed to all topics ")
+
+
+def on_publish(client,userdata,result):     #create function for callback
+   #print("Data published to topic", topic_pub)
+   pass
+
+#Creating an instance and setting up the callbacks
+
+client = mqtt.Client() #creating an instance 
+client.on_connect = on_connect    #on_connect callback
+client.on_message = on_message    #on_message callback 
+client.on_publish = on_publish    #on_publish callback    
+client.on_subscribe = on_subscribe
+#client.tls_set(path_to_certificate)    #establishing the SSL certificate
+client.connect(broker_address, port)    #connecting to broker
+
+#Setting up the subscribe 
+client.subscribe(topic_sub)
+data_pub = ""
+
+client.loop_start() 
+
+t = Thread(target=rmt_ctrl)
+t.start()  
+
+data_pub = "1test/trial"
+
+while 1:
+
+    if clients_connected == 2 and rmt_ctrl_start:
+        time.sleep(2)
+        print ("Ready to start")
+        for i in range(clients_connected):
+            client.publish(client_ID[i],data_pub)
+        time.sleep(5)
+        print("execute ncurses main")
+        rmt_ctrl_start = False   
+        rmt_go = 1
+        
+
+
+client.loop_forever() #loop forever
