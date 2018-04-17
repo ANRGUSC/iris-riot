5df5bbf8a4ce206b87493c046ca3ff25e94bf047
dtormey <dtormey@usc.edu>
2018-04-17 15:01:25 -0700

    added python script for USB forwarding

--- examples/arrest_leader_ctrl_dev/arrest_leader_ctrl.py
+++ examples/arrest_leader_ctrl_dev/arrest_leader_ctrl.py
@@ -2,37 +2,13 @@
 import sys, struct, serial
 import readchar
 
-if len(sys.argv) < 2:
-   print("No device specified.")
-   print("Specify the serial port of the device you wish to connect to.")
-   print("Example:")
-   print("   pololu_cmd_tx.py /dev/ttyUSB0")
-else:
-   ser = serial.Serial(sys.argv[1], 115200)
-   ser.flushInput()
-   print ("Enter your commands below.\r\nInsert 'x' to leave the application.")
-   input=1
-   while 1:
-   # get keyboard input
-      input = readchar.readchar()
-      # Python 3 users
-      # input = input(">> ")
-      if input == 'x':
-        ser.close()
-        exit()
-      else:
-        ser.write(struct.pack('B', ord(input)))
+ser = serial.Serial('/dev/tty.usbserial-A504QPCN', 115200)
+ser2 = serial.Serial('/dev/tty.usbserial', 115200)
+ser.flushInput()
 
-      # elif input == 'w':
-      #   ser.write(struct.pack('B', 119))
-      # elif input == 'a':
-      #   ser.write(struct.pack('B', 97))
-      # elif input == 's':
-      #   ser.write(struct.pack('B', 115))
-      # elif input == 'd':
-      #   ser.write(struct.pack('B', 100))
-        # w=119 a=97 s=115 d=100
+while 1:
 
-# except (KeyboardInterrupt):
-#    ser.close()
-#    print("All done!")
+  print("Ready to Read boys")
+  input2 = ser2.read()
+  print("Read Byte: " + input2)
+  ser.write(struct.pack('B', ord(input2)))
